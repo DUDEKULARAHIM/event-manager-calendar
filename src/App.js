d759d27f4ce02c88e59c1c6f7e8be019aee915c2
Dudekula Rahim <119217092+DUDEKULARAHIM@users.noreply.github.com>
2025-02-26 12:52:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@
 //     <div className="App">
 //       <header className="App-header">
 //         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
 //     </div>
 //   );
 // }
